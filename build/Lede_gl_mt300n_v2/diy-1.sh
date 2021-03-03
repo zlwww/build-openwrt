053ef09c4274ff119318e02d143ee34cc210dfb6
ixxmu <ixxmu@users.noreply.github.com>
2021-03-03 16:03:38 +0800

    Update diy-1.sh

--- build/Lede_gl_mt300n_v2/diy-1.sh
+++ build/Lede_gl_mt300n_v2/diy-1.sh
@@ -11,7 +11,7 @@
 
 # 增加出国源
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-# Uncomment a feed source
+# Uncomment a feed source，如果用lede的源码只用加上这一句就可以看见ssr插件了！不用再次添加源的
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/diy/luci-theme-edge  #主题-edge
@@ -24,3 +24,20 @@ git clone https://github.com/garypang13/luci-app-eqos.git package/diy/luci-app-e
 git clone https://github.com/jerrykuku/node-request.git package/diy/node-request  #京东签到依赖
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/diy/luci-app-jd-dailybonus  #京东签到
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/diy/luci-app-poweroff  #关机（增加关机功能）
+
+#增加软件，这里只是一个示范，这里不需要sfe，因为lede的源码是自带的有，如果你要用天灵的源码（immortalwrt）还想要这个sfe软件那就在那里加上，这里仅仅是作为一个示范！
+#读到的说明书是sfe支持4.14内核，4.19和5.4内核，都可以！ 三大源码其实最大的特色就是内核不一样，lede 内核5.4且最新，天灵18.07是master（4.14），lieonl是19.07master（应该是4.19）
+#三个源码对应的是文件夹里的LEDE、Lieonl和Project ！这下知道差别了吧
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-sfe package/diy/luci-app-sfe
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev package/diy/shadowsocksr-libev
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt package/diy/pdnsd-alt
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/diy/microsocks
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package/diy/dns2socks
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs package/diy/simple-obfs
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package/diy/v2ray-plugin
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray package/diy/v2ray
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan package/diy/trojan
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks package/diy/ipt2socks
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/diy/redsocks2
+
