07e2554a895adf38f4e5ae9271186ee57d7318f6
ixxmu <ixxmu@users.noreply.github.com>
2021-03-04 09:25:20 +0800

    Update diy-1.sh

--- build/Project_x86_64/diy-1.sh
+++ build/Project_x86_64/diy-1.sh
@@ -9,3 +9,4 @@
 # 拉取插件请看《各种命令的简单介绍》第4条、第5条说明,不管大神还是新手请认真的看看,再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
 #如果你想用sfe加速
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-sfe package/diy/luci-app-sfe
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-flowoffload package/diy/luci-app-flowoffload
