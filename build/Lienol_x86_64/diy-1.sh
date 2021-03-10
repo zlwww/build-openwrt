f6d52b8a488a263302a21dff7a8b12e0d6917a68
ixxmu <ixxmu@users.noreply.github.com>
2021-03-10 14:18:04 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -15,7 +15,12 @@
 # svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-flowoffload package/diy/luci-app-flowoffload
 
 #真的是每一个都不一样，lienol维护的是19.07的主分支，如果需要添加passwall添加下面2行就可以，第二个是依赖，跟新的也算频繁，貌似很稳定，该有的插件都有了
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # 下面这个据说是官方支持的passwall，最好和上面的不要同时启用，怕冲突！！！
 #sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+
+# 这里展示的是另外一种拉取方式，可能更加稳妥？
+git clone https://github.com/xiaorouji/openwrt-passwall package/diy/luci-app-passwall
+# 可能再加一个官方ssrplus
+git clone https://github.com/fw876/helloworld package/diy/helloworld
