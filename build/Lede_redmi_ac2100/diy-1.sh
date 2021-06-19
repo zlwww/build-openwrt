2862c4c86a5bcd373f5de1074d262848762289ba
zlwww <32141949+zlwww@users.noreply.github.com>
2021-06-19 15:44:22 +0800

    Update diy-1.sh

--- build/Lede_redmi_ac2100/diy-1.sh
+++ build/Lede_redmi_ac2100/diy-1.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
-rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 # 基本不需要添加啥插件了,我搜集了各位大神的插件都集成一个软件包直接打入源码里面了
 # 要了解增加了什么东西，就到我的专用软件包里面看看看吧，如果还是没有你需要的插件，请不要一下子就拉取别人的插件包
 # 相同的文件都拉一起，因为有一些可能还是其他大神修改过的容易造成编译错误的
